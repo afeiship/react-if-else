14d8557a8246543a2957586f0955170e1994e40d
afeiship <1290657123@qq.com>
2024-03-25 17:18:09 +0800

    feat: react not used

--- packages/lib/src/index.tsx
+++ packages/lib/src/index.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import { Component } from 'react';
 import ReactConditionManager, { ReactConditionManagerProps } from '@jswork/react-condition-manager';
 
 export interface ReactIfElseProps extends Omit<ReactConditionManagerProps, 'items'> {
